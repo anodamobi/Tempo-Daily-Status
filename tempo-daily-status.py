a08d2a3ce225defae98164cee5a4c24b486747a4
Alex Zavrazhniy <alex@anoda.mobi>
2014-12-23 12:49:41 +0200

    config structure update; working with config parser

--- tempo-daily-status.py
+++ tempo-daily-status.py
@@ -1,3 +1,32 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 __author__ = 'themengzor'
+
+import requests
+import ConfigParser
+
+configFile = ConfigParser.ConfigParser()
+
+try:
+    configFile.read('tds.conf')
+except:
+    print 'Unable to read config file ./tds.conf'
+    exit(1)
+
+globalConfig = {}
+smtpConfig = {}
+tempoConfig = {}
+
+try:
+    globalConfig['emails'] = configFile.get('global', 'emails')
+    smtpConfig['server'] = configFile.get('smtp', 'server')
+    smtpConfig['port'] = configFile.get('smtp', 'port')
+    smtpConfig['security'] = configFile.get('smtp', 'security')
+    smtpConfig['auth'] = configFile.get('smtp', 'auth')
+    smtpConfig['login'] = configFile.get('smtp', 'login')
+    smtpConfig['password'] = configFile.get('smtp', 'password')
+    tempoConfig['baseurl'] = configFile.get('tempo', 'baseurl')
+except:
+    print 'Unable to parse config file'
+    exit(1)
+
