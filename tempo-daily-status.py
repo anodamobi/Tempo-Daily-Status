cc48d9deacfe6a4c29be48f3457eaec0287dd6fe
Alex Zavrazhniy <alex@anoda.mobi>
2014-12-24 16:07:14 +0200

    pre-release; finished template; added email sending;

--- tempo-daily-status.py
+++ tempo-daily-status.py
@@ -6,11 +6,13 @@ import requests
 import ConfigParser
 from datetime import date
 from xml.etree import ElementTree
-from pprint import pprint
 from jinja2 import Template
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-# today = date.today()
-today = date(2014,12,18)
+today = date.today()
+
 configFile = ConfigParser.ConfigParser()
 
 def tree2dict(element):
@@ -34,13 +36,14 @@ tempoConfig = {}
 
 # reading config
 try:
-    globalConfig['emails'] = configFile.get('global', 'emails')
+    globalConfig['emails'] = [x.strip() for x in configFile.get('global', 'emails').split(',')]
     smtpConfig['server'] = configFile.get('smtp', 'server')
     smtpConfig['port'] = configFile.get('smtp', 'port')
     smtpConfig['security'] = configFile.get('smtp', 'security')
     smtpConfig['auth'] = configFile.get('smtp', 'auth')
     smtpConfig['login'] = configFile.get('smtp', 'login')
     smtpConfig['password'] = configFile.get('smtp', 'password')
+    smtpConfig['from'] = configFile.get('smtp', 'from')
     tempoConfig['baseurl'] = configFile.get('tempo', 'baseurl')
     tempoConfig['token'] = configFile.get('tempo', 'token')
 except Exception as e:
@@ -94,13 +97,40 @@ for worklog in worklogs:
     users[worklog['username']]['stats']['total_worklogs'] += 1
     users[worklog['username']]['stats']['workload'] = min(8, users[worklog['username']]['stats']['total_hours']) / 8 * 100
 
-pprint(users)
-
 data = {
     'date': today
 }
 
 template = Template(open('templates/daily-report.html').read())
 result = template.render(users=users, globalConfig=globalConfig, tempoConfig=tempoConfig, data=data)
-open('result.html', 'w+').write(result.encode('UTF-8'))
-print result
+
+# preparing message
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Your team TEMPO worklogs"
+msg['From'] = 'Tempo Daily Status <%s>' % smtpConfig['from']
+msg['To'] = ", ".join(globalConfig['emails'])
+
+msg.attach(MIMEText('This is HTML email', 'plain'))
+msg.attach(MIMEText(result.encode('UTF-8'), 'html'))
+
+# sending email
+if smtpConfig['security'] == 'ssl':
+    mail = smtplib.SMTP_SSL()
+else:
+    mail = smtplib.SMTP()
+
+# mail.set_debuglevel(1)
+
+try:
+    mail.connect(smtpConfig['server'], smtpConfig['port'])
+    mail.login(smtpConfig['login'], smtpConfig['password'])
+    mail.sendmail(msg['From'], globalConfig['emails'], msg.as_string())
+    mail.quit()
+except Exception as e:
+    print 'Unable to send email:'
+    print e.message
+    exit(1)
+else:
+    print 'Email sent to', globalConfig['emails']
+
+# EOF
